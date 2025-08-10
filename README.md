a125e47d74577ed3cbeacd37c324bfe8d999f1f3
Toprak Ates <toprakateskir31@gmail.com>
2025-08-10 18:49:20 +0300

    sh: line 1: wq: command not found
    sh: line -2: $'\017\030': command not found

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
+<<<<<<< HEAD
 # aeSuggestion
 A basic suggestion bot. I'll do some updates soon.
+=======
+## Installation
+- Make sure Python 3.8+ is installed.
+- Install required libraries:
+   ```bash
+   pip install -r requirements.txt
+```
+
+## Kurulum
+- Python 3.8+ yüklü olmalı.
+- Gerekli kütüphaneleri yükle:
+   ```bash
+   pip install -r requirements.txt
+```
+>>>>>>> 16fdae2 (Added bot codes.)
---
+++ bot.py
@@ -0,0 +1,59 @@
+import discord
+from discord.ext import commands
+import os
+from dotenv import load_dotenv
+
+#
+#╔══════════════════════╗
+#║      discord:        ║
+#║   .gg/toprakates     ║
+#╚══════════════════════╝
+#
+
+load_dotenv()  # .env dosyasına tokeninizi girin
+TOKEN = os.getenv("DISCORD_TOKEN")
+
+# Oylama için kullanacağınız kanal id'sini yazın.
+OY_KANALI_ID = 1404119145716514929
+
+intents = discord.Intents.default()
+intents.message_content = True
+intents.messages = True
+intents.reactions = True
+
+bot = commands.Bot(command_prefix="!", intents=intents)
+
+@bot.event
+async def on_ready():
+    print(f"Giriş yapıldı: {bot.user}")
+
+@bot.event
+async def on_message(message):
+    if message.author.bot:
+        return
+
+    if message.channel.id != OY_KANALI_ID:
+        return
+
+    await message.delete()
+
+    embed = discord.Embed(
+        title="🗳️ Oylama Başladı!",
+        description=message.content,
+        color=discord.Color.blue()
+    )
+    embed.set_footer(
+        text=f"Gönderen: {message.author.display_name}",
+        icon_url=message.author.avatar.url if message.author.avatar else None
+    )
+
+    embed_message = await message.channel.send(embed=embed)
+
+    await embed_message.add_reaction("✅")
+    await embed_message.add_reaction("❌")
+
+    await bot.process_commands(message)
+
+bot.run(TOKEN)
+
+#
